f1584cabeec59c5ceddc5f935d2bae56b2c2a34d
Tiger-ghost <panxiaotiger@gmail.com>
2019-10-01 19:40:44 +0800

    Site updated: 2019-10-01 19:40:28

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2018":{"7":{"28":true}},"2019":{"3":{"29":true},"5":{"20":true},"6":{"26":true}}}';
+      var data = '{"2018":{"7":{"28":true}},"2019":{"3":{"29":true},"5":{"20":true},"6":{"26":true},"8":{"25":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
